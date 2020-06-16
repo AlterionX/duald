f85ede0274eb381d16dc5f943d54254d9794f4eb
Benjamin Xu <ben.xu.cs@gmail.com>
2020-06-16 15:26:07 -0500

    Get the cursor

---
+++ src/editor.rs
@@ -0,0 +1,135 @@
+use std::{
+    ops::{Bound, RangeBounds},
+    collections::HashMap,
+};
+use wasm_bindgen::JsValue;
+use web_sys::{HtmlElement, Range};
+use tap::*;
+
+type BoundTup<T> = (Bound<T>, Bound<T>);
+
+enum Cursor {
+    Select(BoundTup<usize>),
+    Insert(usize),
+    // TODO Overwrite,
+}
+
+pub trait EditorUI: Sized {
+    fn init(editor: HtmlElement) -> Result<Self, JsValue>;
+}
+
+pub trait TextProcessor: Sized {
+    fn init(editor: HtmlElement) -> Result<Self, JsValue>;
+}
+
+pub struct Editor<UI: EditorUI, Processor: TextProcessor> {
+    buffer: String,
+    spans: Vec<BoundTup<usize>>,
+    cursor: Cursor,
+    // TODO figure out how to prevent browser's contenteditable from working
+    html_span_map: HashMap<BoundTup<usize>, BoundTup<usize>>,
+
+    impl_types: std::marker::PhantomData<(UI, Processor)>,
+}
+
+const CURSOR_CHANGE_EVENTS: &'static str = "mousedown mouseup keydown keyup";
+pub const CONTENT_CSS_CLASS: &'static str = "duald_editor_content";
+pub const PRIMARY_CSS_CLASS: &'static str = "duald_content";
+
+impl<UI: EditorUI, Processor: TextProcessor> Editor<UI, Processor> {
+    pub fn init(editor: HtmlElement) -> Result<Self, JsValue> {
+        // TODO create actual editor
+        unimplemented!("Editor not yet implemented.")
+    }
+
+    // Source: https://stackoverflow.com/questions/4811822/get-a-ranges-start-and-end-offsets-relative-to-its-parent-container/4812022#4812022
+    fn find_cursor(editor: HtmlElement) -> Option<Cursor> {
+        // The reference code also has a `document`, but that's only in IE.
+        let doc = editor.owner_document()?;
+        // The `default_view` is called `parent_window` only in IE.
+        let win = doc.default_view()?;
+
+        // Locate selection
+        let sel = match win.get_selection() {
+            Ok(sel @ Some(_))  => sel,
+            Err(_) | Ok(None) => {
+                log::warn!("Failed to get selection from window. Retrying with document.");
+                let sel = match doc.get_selection() {
+                    Ok(sel) => sel,
+                    Err(e) => {
+                        log::error!("The selection could not be detected on document either due to {:?}.", e);
+                        None
+                    },
+                };
+                sel
+            },
+        }?;
+
+        // Convert Selection to Cursor
+        if sel.range_count() > 0 {
+            let sel_range = sel.get_range_at(0).ok()?;
+            let ele_range = {
+                let range = sel_range.clone();
+                range.select_node_contents(&editor)
+                    .ok()?;
+                range
+            };
+
+            let sel_end = {
+                let sel_end = (sel_range.end_container(), sel_range.end_offset());
+                let container = sel_end.0
+                    .tap_err(|e| log::error!("Failed to get end container due to {:?}.", e))
+                    .ok()?;
+                let offset = sel_end.1
+                    .tap_err(|e| log::error!("Failed to get end offset due to {:?}.", e))
+                    .ok()?;
+                (container, offset)
+            };
+            let end_offset = {
+                ele_range.set_end(&sel_end.0, sel_end.1)
+                    .tap_err(|e| log::error!("Failed to set range end selection end due to {:?}.", e))
+                    .ok()?;
+                ele_range.to_string().length()
+            } as usize;
+            
+            if sel.collapsed() {
+                Some(Cursor::Insert(end_offset))
+            } else {
+                let sel_start = {
+                    let sel_start = (sel_range.start_container(), sel_range.start_offset());
+                    let container = sel_start.0
+                        .tap_err(|e| log::error!("Failed to get start container due to {:?}.", e))
+                        .ok()?;
+                    let offset = sel_start.1
+                        .tap_err(|e| log::error!("Failed to get start offset due to {:?}.", e))
+                        .ok()?;
+                    (container, offset)
+                };
+                let start_offset = {
+                    ele_range.set_end(&sel_start.0, sel_start.1)
+                        .tap_err(|e| log::error!("Failed to set range end to selection start due to {:?}.", e))
+                        .ok()?;
+                    ele_range.to_string().length()
+                } as usize;
+                Some(Cursor::Select((
+                    Bound::Included(start_offset),
+                    Bound::Included(end_offset),
+                )))
+            }
+        } else {
+            None
+        }
+    }
+}
+
+pub type DefaultEditor = Editor<(), ()>;
+impl EditorUI for () {
+    fn init(_: HtmlElement) -> Result<Self, JsValue> {
+        Ok(())
+    }
+}
+impl TextProcessor for () {
+    fn init(_: HtmlElement) -> Result<Self, JsValue> {
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue, JsCast};
+use web_sys::Document;
+use tap::*;
+
+mod binding;
+mod editor;
+
+use editor::{Editor, EditorUI, TextProcessor, DefaultEditor};
+
+#[wasm_bindgen]
+pub fn attach_default_editor_to(id: &str, doc: Option<Document>) -> Result<(), JsValue> {
+    let editor: DefaultEditor = attach_to(id, doc)?;
+    Ok(())
+}
+
+fn attach_to<UI: EditorUI, Processor: TextProcessor>(id: &str, doc: Option<Document>) -> Result<Editor<UI, Processor>, JsValue> {
+    let elem = doc
+        .tap_none(|| log::info!("No document provided. Attempting to use default document."))
+        .or_else(|| web_sys::window()?.document())
+        .ok_or_else(|| "No default document found. Duald is likely not running in a web browser environment.".to_string())
+        .tap_err(|e| log::error!("Editor failed to init: {}", e))?
+        .query_selector(format!("#{}.{}", id, editor::PRIMARY_CSS_CLASS).as_str())
+        .tap_err(|e| log::error!("Error failed to init: {:?}", e))?
+        .ok_or_else(|| format!("Failed to located targeted editor element. Note that it is also required to give the targeted element the `{}` CSS class.", editor::PRIMARY_CSS_CLASS))
+        .tap_err(|e| log::error!("Error failed to init: {}", e))?
+        .dyn_into()
+        .map_err(|_| "Located element is not an html element.")
+        .tap_err(|e| log::error!("Error failed to init: {}", e))?;
+    // TODO do something with the editor
+    Editor::<UI, Processor>::init(elem)
+}
+
 #[cfg(test)]
 mod tests {
 }
