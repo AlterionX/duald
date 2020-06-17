34890868d195c1bdd5abcaa36fdb9395488b12c0
Benjamin Xu <ben.xu.cs@gmail.com>
2020-06-17 16:02:40 -0500

    Update build_and_run.sh
    Add support for additional args to `http` and `wasm-pack`

--- test_server/build_and_run.sh
+++ test_server/build_and_run.sh
@@ -2,12 +2,62 @@
 
 set -eu;
 
+VERBOSITY=0;
 SCRIPT_ROOT="${0%/*}"; # if this causes problems, use "$(dirname -- $0)"
+SERVER_ARGS="";
+BUILD_ARGS="";
+while [ $# -ne 0 ]; do
+    case "$1" in
+    --server)
+        while [ $# -ne 1 ] && [ "$2" != "--" ]; do
+            SERVER_ARGS+="$SERVER_ARGS $2";
+            shift;
+        done
+        shift;
+        if [ $# -ne 0 ] && [ "$1" != "--" ]; then
+            shift;
+        fi
+        ;;
+    --build)
+        while [ $# -ne 1 ] && [ "$2" != "--" ]; do
+            BUILD_ARGS="$BUILD_ARGS $2";
+            shift;
+        done
+        if [ $# -ne 0 ] && [ "$1" != "--" ]; then
+            shift;
+        fi
+        shift;
+        ;;
+    -v)
+        VERBOSITY=1;
+        shift;
+        ;;
+    *) # unsupported flags
+        echo "Error: Unsupported flag $1" >&2;
+        exit 1;
+        ;;
+    esac
+done
+
+if [ $VERBOSITY -ne 0 ]; then
+    echo "Server args:${SERVER_ARGS}";
+    echo "Build args:${BUILD_ARGS}";
+fi
 
 cd $SCRIPT_ROOT;
 echo "Building test_server from ${SCRIPT_ROOT}.";
-wasm-pack build ../duald --out-dir ../test_server/pkg --target web;
+if [ ${#BUILD_ARGS[@]} -eq 0 ]; then
+    wasm-pack build ../duald --out-dir ../test_server/pkg --target web;
+else
+    echo "Custom build arguments: ${BUILD_ARGS}.";
+    wasm-pack build ../duald --out-dir ../test_server/pkg --target web ${BUILD_ARGS};
+fi
 echo "Built test_server. Launching...";
-http -a 127.0.0.1;
+if [ ${#SERVER_ARGS[@]} -eq 0 ]; then
+    http -a 127.0.0.1;
+else
+    echo "Custom build arguments: ${SERVER_ARGS}.";
+    http ${SERVER_ARGS};
+fi
 echo "test_server shutdown."
 cd -;
